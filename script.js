0de696de81502bc202760786ea3c5a2275d2fb41
Gamessteps <catarisanogiuseppe08@gmail.com>
2025-08-07 03:48:41 +0200

    script.js

--- script.js
+++ script.js
@@ -2,20 +2,27 @@ const clientId = '1fw8yuj9fmmw5vp9v2u5zpfiwiwesu';
 const accessToken = 'cs4sk9zml93lj88m0fxzeayf3fqc8n';
 
 const streamers = [
-  {name: "ermixone_", url: "https://www.twitch.tv/ermixone_"},
-  {name: "sicarium250", url: "https://www.twitch.tv/sicarium250"},
-  {name: "andrijpg_", url: "https://www.twitch.tv/andrijpg_"},
-  {name: "str3gons__", url: "https://www.twitch.tv/str3gons__"},
-  {name: "th3blink", url: "https://www.twitch.tv/th3blink"},
-  {name: "metargon", url: "https://www.twitch.tv/metargon"},
-  {name: "imnicoo_", url: "https://www.twitch.tv/imnicoo_"},
-  {name: "imgiovyy", url: "https://www.twitch.tv/imgiovyy"},
-  {name: "less16__", url: "https://www.twitch.tv/less16__"},
-  {name: "poenico", url: "https://www.twitch.tv/poenico"},
-  {name: "subbo_00", url: "https://www.twitch.tv/subbo_00"},
-  {name: "blazecube_21", url: "https://www.twitch.tv/blazecube_21"},
-  {name: "nocturniverse", url: "https://www.twitch.tv/nocturniverse"},
-  {name: "d3struct10ntv", url: "https://www.twitch.tv/d3struct10ntv"},
+  { name: "ermixone_", url: "https://www.twitch.tv/ermixone_" },
+  { name: "sicarium250", url: "https://www.twitch.tv/sicarium250" },
+  { name: "andrijpg_", url: "https://www.twitch.tv/andrijpg_" },
+  { name: "str3gons__", url: "https://www.twitch.tv/str3gons__" },
+  { name: "th3blink", url: "https://www.twitch.tv/th3blink" },
+  { name: "metargon", url: "https://www.twitch.tv/metargon" },
+  { name: "imnicoo_", url: "https://www.twitch.tv/imnicoo_" },
+  { name: "imgiovyy", url: "https://www.twitch.tv/imgiovyy" },
+  { name: "less16__", url: "https://www.twitch.tv/less16__" },
+  { name: "poenico", url: "https://www.twitch.tv/poenico" },
+  { name: "subbo_00", url: "https://www.twitch.tv/subbo_00" },
+  { name: "blazecube_21", url: "https://www.twitch.tv/blazecube_21" },
+  { name: "nocturniverse", url: "https://www.twitch.tv/nocturniverse" }, // lolller & Just a Chill Guy
+  { name: "d3struct10ntv", url: "https://www.twitch.tv/d3struct10ntv" },
+  { name: "noluuke", url: "https://www.twitch.tv/noluuke" },
+  { name: "keenzoo__", url: "https://www.twitch.tv/keenzoo__" },
+  { name: "alb3x_official", url: "https://www.twitch.tv/alb3x_official" },
+  { name: "maxibinn", url: "https://www.twitch.tv/maxibinn" },
+  { name: "sussosioilpazzo", url: "https://www.twitch.tv/sussosioilpazzo" },
+  { name: "itzmezii", url: "https://m.twitch.tv/itzmezii/" },
+  { name: "hemlock_noir_", url: "https://www.twitch.tv/hemlock_noir_" }
 ];
 
 const liveContainer = document.getElementById('live-streams');
